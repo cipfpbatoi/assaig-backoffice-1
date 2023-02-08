53c5a663b993c063e220b35079bfb91a7f564573
Adria <adria.jorda.m@gmail.com>
2023-02-08 18:46:23 +0100

    [add] reserva controller

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -12,21 +12,22 @@ class ReservaController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        //
+        $reservas = Reserva::paginate(10);
+        return view('reserva.index', compact('reservas'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        return view('reservas.store');
     }
 
     /**
@@ -37,7 +38,21 @@ class ReservaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $reserva = new Reserva();
+        $reserva->nombre = $request->nombre;
+        $reserva->email = $request->email;
+        $reserva->telefono = $request->telefono;
+        $reserva->comensales = $request->comensales;
+        $reserva->observaciones = $request->observaciones;
+        $reserva->localizador = $request->localizador;
+        $reserva->confirmada = $request->confirmada;
+
+        try {
+            $reserva->save();
+            return response()->json($reserva, 201);
+        }catch (QueryException $e){
+            return response()->json([$e->getMessage()], 400);
+        }
     }
 
     /**
@@ -48,7 +63,8 @@ class ReservaController extends Controller
      */
     public function show(Reserva $reserva)
     {
-       //
+        $reservaToFind = Reserva::findOrFail($reserva->id);
+        return view('reserva.show', compact('reservaToFind'));
     }
 
     /**
@@ -59,7 +75,8 @@ class ReservaController extends Controller
      */
     public function edit(Reserva $reserva)
     {
-       //
+        $reservaToFind = Reserva::findOrFail($reserva->id);
+        return view('reserva.edit', compact($reservaToFind));
     }
 
     /**
@@ -71,7 +88,17 @@ class ReservaController extends Controller
      */
     public function update(Request $request, Reserva $reserva)
     {
-        //
+        $reservaToEdit = Reserva::find($reserva->id);
+        $reservaToEdit->nombre = $request->nombre;
+        $reservaToEdit->email = $request->email;
+        $reservaToEdit->telefono = $request->telefono;
+        $reservaToEdit->comensales = $request->comensales;
+        $reservaToEdit->observaciones = $request->observaciones;
+        $reservaToEdit->localizador = $request->localizador;
+        $reservaToEdit->confirmada = $request->confirmada;
+        $reservaToEdit->save();
+
+        return view('reserva.show', compact('reservaToEdit'));
     }
 
     /**
@@ -82,6 +109,8 @@ class ReservaController extends Controller
      */
     public function destroy(Reserva $reserva)
     {
-        //
+        $reservaToDelete = Reserva::find($reserva->id);
+        $reservaToDelete->delete();
+        $this->index();
     }
 }
