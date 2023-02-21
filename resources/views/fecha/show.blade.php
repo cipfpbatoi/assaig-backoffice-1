89ae1cff68c47701cdfbc2aa06ee84c49398d1ab
Veddartha1 <spayam82@gmail.com>
2023-02-21 08:53:40 +0100

    Actualización de .gitignore

--- resources/views/fecha/show.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout.layout')
-@section('title', "L'assaig - " . $titulo)
-@section('content')
-    @include('partials.breadcrumb', ['breadcrumbs' => $breadcrumbs])
-    <section class="row single-section mx-auto my-5 g-0">
-    </section>
