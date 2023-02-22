2edc0776cf08d7cc7024434ccbb11fcdc0078f13
Veddartha1 <spayam82@gmail.com>
2023-02-22 19:28:11 +0100

    add favicon

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,6 +5,7 @@
     <title>
         @yield('title')
     </title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -12,7 +13,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Lexend&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.dataTables.min.css">
-    <link rel="icon" type="image/ico" href="{{ asset('img/favicon.ico') }}">
     <link rel="stylesheet" href="/css/estilos.css">
 </head>
 <body class="container-fluid">
