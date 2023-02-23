33d86135cee9f92e17ac9551007477d9fbe160ed
Veddartha1 <spayam82@gmail.com>
2023-02-23 13:38:22 +0100

    footer fixed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="row text-center py-5 bg-dark text-center text-white mt-5">
     <section class="col-12 single-section mx-auto pt-5">
-        <div class="row">
-            <div class="py-3 col-md-4 col-12 text-md-start text-center">
+        <div class="row px-2">
+            <div class="col-md-6 col-12 text-md-start text-center">
                 © L'assaig 2023 | Todos los derechos reservados.
             </div>
-            <div class="py-3 offset-md-4 col-md-4 col-12  text-md-end text-center">
+            <div class="offset-md-6 col-md-4 col-12  text-md-end text-center">
                 Diseño y desarrollo:
                 <a href="https://portal.edu.gva.es/cipfpbatoi/">cipfpbatoi</a>
             </div>
